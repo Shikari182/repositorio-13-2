8061c81bf15a99adbf2416bfdd4acf3d25dba05f
Enrique <enriqueShikari@gmail.com>
2025-06-27 12:30:59 +0200

    Commit

--- src/pages/account/account.vm.ts
+++ src/pages/account/account.vm.ts
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Account } from "./api/account.api-model";
 import { saveAccount } from "./api/account.api";
 import { appRoutes } from "@/core/router";
 import { validateForm } from "./account.validations";
-
+import { mapFormToApi } from "./account.mapper";
 
 export interface AccountFormErrors {
   type?: string;
@@ -14,37 +13,38 @@ export interface AccountFormErrors {
 export interface AccountFormVm {
   type: string;
   name: string;
-  errors: {
-    type?: string;
-    name?: string;
-  };
+  errors: AccountFormErrors;
   setType: (type: string) => void;
   setName: (name: string) => void;
   handleSubmit: () => void;
 }
 
 export const useAccountForm = (): AccountFormVm => {
-  const [type, setType] = useState("");
-  const [name, setName] = useState("");
-  const [errors, setErrors] = useState<{ type?: string; name?: string }>({});
+  const [type, setType] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [errors, setErrors] = useState<AccountFormErrors>({});
   const navigate = useNavigate();
 
   const handleSubmit = () => {
+
     const newErrors = validateForm({ type, name });
-    if (!type) {
-      newErrors.type = "Debe elegir un tipo de cuenta";
-    }
-    if (!name) {
-      newErrors.name = "Debe introducir un alias";
-    }
     setErrors(newErrors);
+
+
     if (Object.keys(newErrors).length === 0) {
-      const account: Account = { type, name };
-      saveAccount(account).then(() => {
+      const apiPayload = mapFormToApi({ type, name });
+      saveAccount(apiPayload).then(() => {
         navigate(appRoutes.accountList);
       });
     }
   };
 
-  return { type, name, errors, setType, setName, handleSubmit };
+  return {
+    type,
+    name,
+    errors,
+    setType,
+    setName,
+    handleSubmit,
+  };
 };
